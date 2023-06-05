3f64f25f41c8d45c65fff5aa85087d7c1c744c3b
Manyam Sanjay Kumar Reddy <94047241+ManyamSanjayKumarReddy@users.noreply.github.com>
2023-06-05 14:51:01 +0530

    Day 7 Update

--- src/Strings/A.java
+++ src/Strings/A.java
@@ -3,30 +3,24 @@ package Strings;
 import java.util.Scanner;
 
 /*
-        Notes
+        Notes :
+
         -- Java is an Object-Oriented Programming Language - Contains Classes, Objects.
+        -- Class is Just a blueprint.
+        -- Objects are known as the Physical Things of the Classes.
+        -- We have to write class in any program for the security of the code.
+        -- Main is the keyword which we people designed to run in any of the online Compiler.
+        -- And also Public is also required
+        -- Now we have to receive the input.
+        --
  */
-public class A {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String l = sc.next();
-        System.out.println(reversedString(l));
-    }
 
-    static String reversedString(String str){
-        char[] stringArray = str.toCharArray();
-        int start = 0;
-        int end = stringArray.length - 1;
-
-        while (start < end){
-            char temp = stringArray[start];
-            stringArray[start] = stringArray[end];
-            stringArray[end] = temp;
-
-            start++;
-            end--;
+public class A{
+    public static void main(String[] marker){
+        Scanner bramhanandam = new Scanner(System.in);
+        String S = bramhanandam.nextLine();
+        for (int car = S.length() - 1; car >= 0; car--){
+            System.out.print(S.charAt(car));
         }
-
-        return new String(stringArray);
     }
 }
