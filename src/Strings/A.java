439477342f823c5e9979bd055bf238fe9abbac8d
Manyam Sanjay Kumar Reddy <94047241+ManyamSanjayKumarReddy@users.noreply.github.com>
2023-06-05 14:23:00 +0530

    Day 7 Update

--- src/Strings/A.java
+++ src/Strings/A.java
@@ -3,7 +3,8 @@ package Strings;
 import java.util.Scanner;
 
 /*
-        Reverse a string
+        Notes
+        -- Java is an Object-Oriented Programming Language - Contains Classes, Objects.
  */
 public class A {
     public static void main(String[] args){
