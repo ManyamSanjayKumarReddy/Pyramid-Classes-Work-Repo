de83c4f67ce094487a94daff75141b341deb541d
Manyam Sanjay Kumar Reddy <94047241+ManyamSanjayKumarReddy@users.noreply.github.com>
2023-06-06 12:12:28 +0530

    Day 8 Update

---
+++ src/Day_8/HashMap/b.java
@@ -0,0 +1,19 @@
+package Day_8.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class b{
+    public static void main(String args[]){
+        HashMap<Integer,String> map=new HashMap<Integer,String>();//Creating HashMap
+        map.put(1,"Mango");  //Put elements in Map
+        map.put(2,"Apple");
+        map.put(3,"Banana");
+        map.put(1,"Grapes"); //trying duplicate key
+
+        System.out.println("Iterating Hashmap...");
+        for(Map.Entry m : map.entrySet()){
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
+    }
+}
