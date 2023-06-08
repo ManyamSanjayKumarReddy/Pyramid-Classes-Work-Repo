6b4f15569202204b86db599a64883d114ce2d081
Manyam Sanjay Kumar Reddy <94047241+ManyamSanjayKumarReddy@users.noreply.github.com>
2023-06-08 22:30:59 +0530

    Day 10 Update

--- src/Stack_Queues/C.java
+++ src/Stack_Queues/C.java
@@ -1,13 +1,45 @@
 package Stack_Queues;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class C {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        Stack<Integer> stk = new Stack<>();
+        int k = 0;
+        int T = sc.nextInt();
+        while (T-- != 0) {
 
+            k = k + 1;
+            System.out.println("Case " + k + ":");
+
+            int n;
+            Stack<Integer> maxStk = new Stack<>();
+            n = sc.nextInt();
+            while (n-- != 0){
+                char cmd;
+                cmd = sc.next().charAt(0);
+                if (cmd == 'A') {
+                    int x;
+                    x = sc.nextInt();
+                    if (maxStk.empty()) maxStk.push(x);
+                    else {
+                        if (maxStk.peek() < x){maxStk.push(x);}
+                        else maxStk.push(maxStk.peek());
+                    }
+                } else if (cmd == 'R') {
+                    if (!maxStk.empty()){
+                        maxStk.pop();
+                    }
+                }
+                else {
+                    if (maxStk.empty()) {
+                        System.out.println("Empty");
+                    }
+                    else System.out.println(maxStk.peek());
+                }
+
+            }
+        }
     }
+
 }
