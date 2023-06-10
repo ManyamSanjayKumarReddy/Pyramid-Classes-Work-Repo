d9fa6e162babc8099d25a301c3e7af563eec2510
Manyam Sanjay Kumar Reddy <94047241+ManyamSanjayKumarReddy@users.noreply.github.com>
2023-06-10 21:01:51 +0530

    Day 11 Update

--- src/Accenture_1/C.java
+++ src/Accenture_1/C.java
@@ -12,14 +12,37 @@ public class C {
 
         String bike = scanner.nextLine().toLowerCase();
 
-        String features = scanner.nextLine().toLowerCase();
+        String[] features = new String[3];
+        for (int i = 0; i < 3; i++){
+            features[i] = scanner.next();
+        }
 
-        if (bike.equals("mountain") && budget >= 500 && features.contains("off-road")) {
-            System.out.println("Mountain Bike - 500");
-        } else if (bike.equals("road") && budget >= 700 && features.contains("lightweight")) {
-            System.out.println("Road Bike - 700");
-        } else if (bike.equals("hybrid") && budget >= 600 && features.contains("multiple gears")) {
-            System.out.println("Hybrid Bike - 600");
+        String[] featureset = {"off-road tires", "suspension system", "multiple gears", "thin tires", "lightweight frame", "versatile"};
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("off-road");
+        list.add("suspension system");
+        list.add("multiple gears");
+        list.add("thin tires");
+        list.add("lightweight frame");
+        list.add("versatile");
+
+
+        if (bike.equals("mountain") && budget >= 500) {
+            if (list.contains(features)){
+                System.out.println("Mountain Bike - 500");
+            }
+        } else if (bike.equals("road") && budget >= 700) {
+
+            if (list.contains(features)){
+                System.out.println("Road Bike - 700");
+            }
+        } else if (bike.equals("hybrid") && budget >= 600) {
+
+            if (list.contains(features)){
+                System.out.println("Hybrid Bike - 600");
+            }
         } else {
             System.out.println("No suitable bicycles found.");
         }
